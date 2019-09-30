498e278597f91283d1f9a480b7bf99349c888607
Josh <josh@plink.co.nz>
2019-09-30 15:05:28 +1300

    Fixed ZSH location

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# Set up scripts
-Add these to `~/.zshrc`
+# Oh My ZSH Setup
+
+```
+curl https://raw.githubusercontent.com/posty72/config-scripts/master/zsh/setup.sh | sudo sh
+```
+
+# Linting configs
+---
+Add these to `~/.zshrc` (should happen automatically with Oh My ZSH setup):
 
 ```bash
 alias addeditorconf='curl -s https://raw.githubusercontent.com/posty72/config-scripts/master/.editorconfig > .editorconfig'
 alias addeslint='curl -s https://raw.githubusercontent.com/posty72/config-scripts/master/.eslintrc > .stylelintrc.json && npm i -D eslint eslint-plugin-babel eslint-plugin-react eslint-plugin-filenames eslint-plugin-import'
 alias addstylelint='curl -s https://raw.githubusercontent.com/posty72/config-scripts/master/.stylelintrc > .stylelintrc && npm i -D stylelint stylelint-order'
 alias addtslint='curl -s https://raw.githubusercontent.com/posty72/config-scripts/master/tslint > tslint.json && npm i -D tslint'
-```
+``` 
--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,5 @@
 # Install zsh
-sudo apt-get install zsh
+apt-get install zsh
 
 # Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -8,7 +8,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -s "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme"
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 # Apply config
-curl -s https://https://raw.githubusercontent.com/posty72/config-scripts/master/zsh/zshrc > ~/.zshrc
+curl https://raw.githubusercontent.com/posty72/config-scripts/master/zsh/zshrc > ~/.zshrc
